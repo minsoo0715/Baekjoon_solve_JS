e01e2150f4429624238ba687fb94dc9f70339dec
WhiteB <whiteblock0715@gmail.com>
2020-10-16 08:37:41 +0900

    ex15649

---
+++ ex15649.js
@@ -0,0 +1,69 @@
+const readline = require("readline");
+
+var scanf = function (input_n) {
+  return new Promise(function (resolve, reject) {
+   const rl = readline.createInterface({
+     input: process.stdin,
+     output: process.stdout
+   });
+   let input = [];
+
+   
+
+   rl.on("line", function(line) {
+ 
+ 
+     let temp =  line.split(' ').map((el) => parseInt(el))
+     input = input.concat(temp);
+ 
+     if(input_n == input.length) rl.close();
+     
+   
+   }).on("close", function() {
+     resolve(input);
+     
+   })
+  })
+}
+
+
+let visit = [];
+let arr = [];
+
+scanf(2).then( (input) => {
+ n = input[0];
+ m = input[1];
+
+ solve(m, n, 0);
+})
+
+
+function solve(m,n, index) {
+  
+
+  if(index == m) {
+      let str = "";
+      for(let i = 0; i<m; i++) {
+          
+          str += `${arr[i]} `;
+      }
+      console.log(str);
+
+      return;
+  
+  }
+
+  for(let i = 1; i<=n; i++) {
+      if(!visit[i-1]) {
+          arr[index] = i;
+          visit[i-1] = true;
+          solve(m, n, index+1);
+          visit[i-1] = false;
+      }
+  }
+
+
+}
+  
+
+    
